f9064a2e222417d26bb57accb3ab69f7b5fcef05
Sparking2 <sparking02@yahoo.com.mx>
2021-01-14 17:29:27 -0600

    2021-01-14

--- Introduction to Audio/Assets/Scripts/FX/MovingObject.cs
+++ Introduction to Audio/Assets/Scripts/FX/MovingObject.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
+    public int speed = 0;
+    public float maxTimer = 10.0f;
+    private float currentTimer = 0.0f;
+    private Vector3 startingPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.forward * speed * Time.deltaTime;
+        currentTimer += Time.deltaTime;
+        if (currentTimer >= maxTimer)
+        {
+            currentTimer = 0.0f;
+            transform.position = startingPosition;
+        }
     }
 }
